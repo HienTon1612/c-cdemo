4eaa6696f53448b4589cffc1197ff4b31c3415fa
Hien Ton <tonnhd0638@fpt.edu.vn>
2017-08-31 19:55:18 +0700

    Developer C

---
+++ main5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(){
+	printf("%i", plus(6, 7));
+}
+int plus(int a, int b){
+	return a + b;
+}
