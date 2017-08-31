9119d4319e1e7317b6a5bc2ad6c91b1080b2b27e
Hien Ton <tonnhd0638@fpt.edu.vn>
2017-08-31 20:34:29 +0700

    Tai leu moi

--- main5.c
+++ main5.c
@@ -4,8 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(){
-	printf("%i", plus(6, 7));
+	printf("%i \n", plus(6, 7));
+	printf("%i", minus(6, 7));
 }
 int plus(int a, int b){
 	return a + b;
+}
+int minus(int a, int b){
+	return a - b;
 }
